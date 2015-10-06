a5ff3273c7aedcc106b23f30745a3390cc0787fd
xavierjordioller <xavier.jordioller@gmail.com>
2015-10-06 14:26:14 -0400

    Ajout timer

--- src/StartUp.java
+++ src/StartUp.java
@@ -2,8 +2,12 @@
 public class StartUp {
 
 	public static void main(String[] args) {
+		long millis = System.currentTimeMillis();
+		
 		Sudoku sudoku = new Sudoku("C://Users//xavie//Desktop//Montreal//ÉTS//LOG320_SDA//TP2//sudoku1.sud");
 		sudoku.solve();
+		
+		System.out.println((System.currentTimeMillis() - millis)/1000.0) ;
 		
 		System.out.println(sudoku);
 	}
