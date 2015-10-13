e70afd2ddeec24e7d030df3d6f99c8f6c685ba71
unknown <AK79880@LOGTI-A3322-21.ens.ad.etsmtl.ca>
2015-10-13 13:12:57 -0400

    Found lots of errors. one or two are still here. algorithm works if you change the first 0 to 2 (need to know why). Algorithm is slower since I modified something to make it work. Will work on it soon (maybe tomorrow?)

--- src/Sudoku.java
+++ src/Sudoku.java
@@ -13,9 +13,37 @@ public class Sudoku {
 
 	Map<Point, List<Integer>> validPositions = new HashMap<Point, List<Integer>>();
 	Stack<Point> history = new Stack<Point>();
-	int currentX = 0;
+	int currentX = 2;
 	int currentY = 0;
 	long startTime;
+	
+	public void printMap() {
+		System.out.println();
+		System.out.println();
+		System.out.println();
+		System.out.println();
+		System.out.println();
+		System.out.println();
+		for(int i = 0; i < 9; i++) {
+			if(i == 3 || i == 6) {
+				System.out.println();
+			}
+			for(int j = 0; j < 9; j++) {
+				if(j == 3 || j == 6) {
+					System.out.print(" ");
+				}
+				System.out.print(String.valueOf(sudokuArray[i][j]));
+			}
+			System.out.println();
+		}
+	/*	try {
+		//	System.in.read();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}*/
+		
+	}
 
 	public Sudoku(String path) {
 		try (BufferedReader br = new BufferedReader(new FileReader(path))) {
@@ -34,17 +62,19 @@ public class Sudoku {
 
 		for (int y = 0; y < 9; y++) {
 			for (int x = 0; x < 9; x++) {
-				for (int i = 0; i < 9; i++) {
+				if (sudokuArray[y][x] != 0)
+					continue;
+				for (int i = 1; i <= 9; i++) {
 					if (isValid(x, y, i)) {
-						if (validPositions.get(new Point(x, y)) == null) {
+						if (validPositions.get(new Point(y, x)) == null) {
 							List<Integer> myList = new ArrayList<Integer>();
 							myList.add(i);
-							validPositions.put(new Point(x, y), myList);
+							validPositions.put(new Point(y, x), myList);
 						} else {
 							List<Integer> myList = validPositions
-									.get(new Point(x, y));
+									.get(new Point(y, x));
 							myList.add(i);
-							validPositions.put(new Point(x, y), myList);
+							validPositions.put(new Point(y, x), myList);
 						}
 					}
 
@@ -58,12 +88,10 @@ public class Sudoku {
 
 	public void solve(boolean exist) {
 		history.push(new Point(currentX, currentY));
-		/*int i = 1;
-		if (exist) {
-			i = sudokuArray[currentY][currentX];
-			i++;
-		}*/
-		for(int i : validPositions.get(new Point(currentX, currentY))) {
+		/*
+		 * int i = 1; if (exist) { i = sudokuArray[currentY][currentX]; i++; }
+		 */
+		for (int i : validPositions.get(new Point(currentY, currentX))) {
 			forward(i);
 		}
 		backtrack();
@@ -75,12 +103,19 @@ public class Sudoku {
 			return; // not able to solve it
 		}
 		history.pop();
-		/*
-		 * if(history.size() == 0) { System.out.println("hello"); }
-		 */
+		
+		if (history.size() == 1) {
+		//	System.out.println("hello");
+		}
+
 		Point previousPoint = history.peek();
 		currentX = previousPoint.x;
 		currentY = previousPoint.y;
+		
+		int temp = sudokuArray[currentY][currentX];
+		sudokuArray[currentY][currentX] = 0;
+		
+		updateMap(false, temp);
 	}
 
 	public void forward(int k) {
@@ -94,6 +129,7 @@ public class Sudoku {
 			System.out.println("Found it");
 
 		}
+		updateMap(true, k);
 		sudokuArray[currentY][currentX] = k;
 		Point nextPoint = nextAvailableCell();
 
@@ -102,6 +138,7 @@ public class Sudoku {
 		}
 		currentX = nextPoint.x;
 		currentY = nextPoint.y;
+		//printMap();
 		solve(false);
 	}
 
@@ -150,18 +187,74 @@ public class Sudoku {
 		}
 		return true;
 	}
-	
-	public void updateMap(boolean forward) {
-		if(forward) {
+
+	public void updateMap(boolean forward, int k) {
+		if (forward) {
+			// colonne
+			for (int i = (currentY + 1); i < 9; i++) {
+				if (validPositions.containsKey(new Point(i, currentX))) {
+					validPositions.get(new Point(i, currentX)).remove(
+							new Integer(k));
+				}
+			}
+
 			// ligne
-			validPositions.containsKey(key)
+			for (int i = (currentX + 1); i < 9; i++) {
+				if (validPositions.containsKey(new Point(currentY, i))) {
+					validPositions.get(new Point(currentY, i)).remove(
+							new Integer(k));
+				}
+			}
+			int rx = (currentX) / 3 + 1;
+			int ry = (currentY) / 3 + 1;
+			int j = currentX + 1;
+			for (int i = currentY; i < 3 * ry; i++) {
+				for (; j < 3 * rx; j++) {
+					Point pointInGrid = new Point(i, j);
+					if (validPositions.containsKey(pointInGrid)) {
+						validPositions.get(pointInGrid).remove(new Integer(k));
+					}
+
+				}
+				j = (rx - 1) * 3;
+			}
 		} else {
-			
+			for (int i = (currentY + 1); i < 9; i++) {
+				if (validPositions.containsKey(new Point(i, currentX))) {
+					if(isValid(currentX,i,k))
+					validPositions.get(new Point(i, currentX)).add(
+							new Integer(k));
+				}
+			}
+
+			for (int i = (currentX + 1); i < 9; i++) {
+				if (validPositions.containsKey(new Point(currentY, i))) {
+					if(isValid(i,currentY,k))
+					validPositions.get(new Point(currentY, i)).add(
+							new Integer(k));
+				}
+			}
+			int rx = (currentX) / 3 + 1;
+			int ry = (currentY) / 3 + 1;
+			int j = currentX + 1;
+			for (int i = currentY; i < 3 * ry; i++) {
+				for (; j < 3 * rx; j++) {
+					Point pointInGrid = new Point(i, j);
+					if (validPositions.containsKey(pointInGrid)) {
+						if (!validPositions.get(pointInGrid).contains(
+								new Integer(k)))
+							if(isValid(j,i,k))
+							validPositions.get(pointInGrid).add(new Integer(k));
+					
+
+					}
+
+				}
+				j = (rx - 1) * 3;
+			}
 		}
 	}
 }
-
-
 
 /*
  * long startTime = System.nanoTime(); System.out.println(); long endTime =
